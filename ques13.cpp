1809b07afbd08a5f2d31e2ce6f8eabf6c243d529
Sandra-Rosa <sandraantony2002@gmail.com>
2023-03-05 20:16:32 +0530

    Upto Armstrong number

--- ques13.cpp
+++ ques13.cpp
@@ -1,22 +1,39 @@
-//C++ Program to Check whether a number is Armstrong number or not
-//371 = 3^3+7^3+1^3
+//Program to check wheather a given number is armstrong or not
+//eg : 407 = 4^3+0^3+7^3;
+//format = abcd = a^n +b^n + c^n + d^n n:number of digits
 #include <iostream>
+#include<math.h>
 using namespace std;
+int len(int n){
+    int len=0;
+    while(n){
+        len++;
+        n /=10;
+    }
+    return len;
+}
+bool isarmstrong(int n,int len){
+    int sum =0;
+    int temp=n;
+    while(temp>1){
+        int digit = temp%10;
+        sum = sum + pow(digit,len);
+        temp = temp/10;
+    }
+    if(sum == n){
+        return true;
+    }else{
+        return false;
+    }
+}
 int main(){
-    int n=371;
-    int rem=n;
-    int result=0;
-    int n1;
-    while(rem!=0){
-        n1 = rem%10;
-        n1 = n1*n1*n1;
-        result += n1;
-        rem = rem/10;
-    }
-    if(result==n){
-        cout<<"This is an amstrong number";
-    }
-    else{
+    int n = 407;
+    int leng = len(n);
+    bool flag = isarmstrong(n,leng);
+    if(flag){
+        cout<<"Armstrong number";
+    }else{
         cout<<"Not an armstrong number";
     }
+    return 0;
 }
